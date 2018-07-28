e76a649eec85783e1066874ab5482168866f1b54
Jiajun Zhu <george.choo@outlook.com>
2018-07-28 16:03:13 +0000

    add a mount as a datasource

--- spark-notebook-start.sh
+++ spark-notebook-start.sh
@@ -2,6 +2,7 @@ docker container rm spark-notebook
 docker container run --name=spark-notebook -it \
        -v ~/workspace:/home/jovyan/workspace:cached \
        -v ~/workspace/python:/opt/pythonlibs/mnt:cached \
+       -v ~/datasource:/home/jovyan/datasource:cached \
        -v ~/.m2:/home/jovyan/.m2:cached \
        -p 8888:8888 \
        georgezhu/spark-notebook start-notebook.sh \
